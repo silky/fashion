22a8a5f4b5dec527ef74d5f5040aea04804842a6
silky <noonsilk+-noonsilk@gmail.com>
2018-06-17 20:25:59 +1000

    Much better variations

--- Cubic-Limit-Series/src/Main.hs
+++ Cubic-Limit-Series/src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GADTs                      #-}
 {-# LANGUAGE NoMonomorphismRestriction  #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 module Main where
 
@@ -8,6 +9,7 @@ import Prelude hiding (lines)
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
 import Diagrams.TwoD.Tilings
+import Data.List
 import Data.List.Split (chunksOf)
 import System.Random (randomRIO)
 import Control.Monad
@@ -56,7 +58,6 @@ cubeWith indicies =
         sublines = map (\i -> cubeParts !! i) indicies
 
 
-
 tile :: [Int] -> Diagram B
 tile xs = cubeWith xs # lw thin
                       # lc gray
@@ -72,12 +73,52 @@ someIndicies = do
     return indicies
 
 
+-- Let's do something else. Let's pick a random set of indicies,
+-- then increment it randomly. Either by:
+--
+--  1. Removing some element
+--  2. Adding some element
+nextSet :: [Int] -> IO [Int]
+nextSet xs = do
+    b :: Int <- randomRIO (0, 1)
+    i <- randomRIO (0, length xs - 1)
+
+    let remaining = [0..length cubeParts - 1] \\ xs
+    shuffled <- runRVar (shuffle remaining) StdRandom
+
+    let forcedDrop = length xs == length cubeParts
+
+    let xs' = if length xs > 1 && (b == 0 || forcedDrop) 
+                then dropIndex i xs 
+                else (xs ++ take 1 shuffled)
+
+    return xs'
+
+
+dropIndex :: Int -> [a] -> [a]
+dropIndex 0 [] = []
+dropIndex i [] = []
+dropIndex 0 xs = drop 1 xs
+dropIndex i xs = let (h, t) = splitAt i xs in h ++ dropIndex i (drop 1 t)
+
+
 d :: IO (Diagram B)
 d =  do
     let chunks  = 20
         items = chunks * chunks
 
-    tiles <- replicateM items (someIndicies >>= return . tile)
+    -- Method 1:
+    --  Random variations in each step
+    --
+    let f (cur, xs) _ = do xs' <- nextSet xs
+                           return (xs' : cur, xs')
+    seqs <- foldM f (return [], [1, 2]) [0.. items - 2] >>= return . fst
+    let tiles = map tile seqs
+
+    -- Method 2:
+    --  Just completely random.
+    --
+    -- tiles <- replicateM items (someIndicies >>= return . tile)
 
     let diag = vcat (map hcat (chunksOf chunks tiles))
 
