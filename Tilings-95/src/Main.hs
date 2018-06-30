5d3b8429f3f731c5366b1fce96331aac509c9152
silky <noonsilk+-noonsilk@gmail.com>
2018-06-30 15:32:23 +1000

    different tiling thing

--- Tilings-95/src/Main.hs
+++ Tilings-95/src/Main.hs
@@ -6,35 +6,65 @@ module Main where
 
 import Diagrams.Prelude
 import Diagrams.Backend.Cairo.CmdLine
-import Diagrams.TwoD.Tilings
+import Data.List.Split (chunksOf)
 
 main :: IO ()
-main = mainWith d
+main = mainWith ( d # frame 0.1 ) >> putStrLn "Done"
 
-
-t :: Tiling
-t = mk3Tiling [6,12,4] 
--- t = semiregular [4,3,4,5] [3,2,1,0]
 
 d :: Diagram B
-d = drawTilingStyled s sf t 20 20
-                                # centerXY 
-                                # pad 1.1
+d = mkCols (map mkRow tss)
   where
-    s = mempty # lw 0
+    t :: Diagram B
+    t = tile # frame 0.09
+             # intrudeEnvelope (0 ^& 0.22)
 
-    col :: TilingPoly -> Colour Double
-    col Triangle  = green
-    col Square    = white
-    col Hexagon   = white
-    col Octagon   = white
-    col Dodecagon = white
+    mkCols = foldl (\d' t' -> d' # snugB <> t' # alignT) mempty
+    mkRow  = foldl (\d' t' -> d' # snugR <> t' # alignL) mempty
 
-    sf p = mempty 
-                # lw 2
+    ts  = take n2 $ repeat t
+    tss = chunksOf n ts
+    n   = 3
+    n2  = n * n
+
+
+tile :: Diagram B
+tile = (((box # snugB # snugR <> t1 # snugB # snugR) 
+      # snugB # snugL <> t2 # snugB # snugL) # snugT # snugL <> t3 # snugT # snugL)
+      # snugT # snugR <> t4 # snugT # snugR
+  where
+    box   = sq -- # deform' 0.01 wibble 
+               # strokeP
+               # lw 0
+               # lc gray
+
+    t1    = tbase
+    t2    = tbase # reflectX
+    t3    = tbase # reflectXY
+    t4    = tbase # reflectY
+    tbase = rht # deform' 0.01 wibble
+                # strokeP
+                # scale 0.4
+                # fc black 
+                # lw 0.1
                 # lc black
-                # fc ( col
-                        . polyFromSides
-                        . length
-                        . polygonVertices
-                        $ p )
+
+
+sq :: Path V2 Double
+sq = square 1
+
+
+rht :: Path V2 Double
+rht = polygon ( with
+        & polyType .~ PolySides 
+        [ 135 @@ deg, 135 @@ deg  ]
+        [ 1        , sqrt 2    , 1]
+      )
+
+
+wibble :: Deformation V2 V2 Double
+wibble = Deformation $ \p ->
+  ((p^._x) + f * cos ((p ^. _y) * tau)) ^& ((p ^. _y) + f * sin ((p ^. _x) * tau))
+    where
+      f = 0.01
+
