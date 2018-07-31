e2b56ecd552b3c3e12a555b1e451ac36546451d4
silky <noonsilk+-noonsilk@gmail.com>
2018-07-31 19:09:37 +1000

    cool

---
+++ Fusion-Compression/README.md
@@ -0,0 +1,5 @@
+# Fusion-Compression
+
+See: [Fusion-Compression](https://github.com/silky/fashion/issues/133).
+
+![](images/a.png)
---
+++ Fusion-Compression/src/Main.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE NoMonomorphismRestriction  #-}
+
+module Main where
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+
+main :: IO ()
+main = mainWith (d # frame 0.2) >> putStrLn "Done!"
+
+
+d :: Diagram B
+d = boxes
+  where
+    boxes    = sq box
+    allBoxes = sq boxes
+
+    sq d = (d # reflectX ||| d)
+              ===
+           (d ||| d # reflectX)
+
+    box = (square 1 <> mconcat [ s # scale (1 - (0.1*s')) | s' <- [0..9] ]
+            <> mconcat lines
+          )
+            # lc gold
+            # bg blue
+            # clipBy (square 1)
+            # withEnvelope (square 1 :: Diagram B)
+
+    lines = [ centerLine # translate (r2 (r, r)) 
+            | r <- map (\s -> 0.5 - 0.1 * s) [ 0..10 ] 
+            ]
+
+    centerLine = ((0.5) ^& (-0.5)) ~~ ((-0.5) ^& 0.5)
+
+    s = square 1 # rotateBy (1/8)
+                 # scaleX 0.9
+                 # rotateBy (-1/8)
+                 # lc gold
+
