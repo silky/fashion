cddad586e7cf21be655a34f4427cb39a093a4a6a
silky <noonsilk+-noonsilk@gmail.com>
2018-09-11 10:50:52 +1000

    contour-maps fst v

---
+++ Contour-Maps/src/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE NoMonomorphismRestriction  #-}
+
+module Main where
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+
+main :: IO ()
+main = mainWith (d # frame 0.2) >> putStrLn "Done!"
+
+
+d :: Diagram B
+d = mempty
+
