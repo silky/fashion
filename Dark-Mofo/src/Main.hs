92e0df477e0e3d398ef4dc226321cd95234b0c97
silky <noonsilk+-noonsilk@gmail.com>
2019-05-09 11:38:17 +1000

    getting there; not great

--- Dark-Mofo/src/Main.hs
+++ Dark-Mofo/src/Main.hs
@@ -16,14 +16,15 @@ applyNTimes :: Int -> (a -> a) -> a -> a
 applyNTimes n f val = foldl (\s e -> e s) val [f | x <- [1..n]]
 
 d :: Diagram B
-d = applyNTimes 8 tree mempty
-
+d = applyNTimes 2 tree mempty
 
 
 tree :: Diagram B -> Diagram B
-tree d = ( (r 10 1 # alignT <> d) # rotateBy (1/8)  # alignL
-        <> (r 1 10 # alignT <> d) # rotateBy (-1/8) # alignR
-         )
+tree d' = ( (r 10 1 # alignR <> d # alignB) # rotateBy (1/8)  # showOrigin # alignL
+         <> (r 10 1 # alignL <> d # alignB) # rotateBy (-1/8) # showOrigin # alignR
+          )
+  where
+    d = d' # scale 0.4
 
 
 base = (rect 10 1 <> rect 1 10)
@@ -31,7 +32,7 @@ base = (rect 10 1 <> rect 1 10)
         # lw none
 
 r x y = rect x y # bg mediumslateblue
-                 # lc red
+                 # lc black
                  # alignT
 
 
