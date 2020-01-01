a8f8f05f9c4f9d209a0401d22b9bdb5794d424c6
silky <noonsilk+-noonsilk@gmail.com>
2020-01-02 08:12:18 +1100

    gala tiles

--- Rect-Sq/src/Main.hs
+++ Rect-Sq/src/Main.hs
@@ -12,8 +12,11 @@ import Nvds.Colours.ColourSets
 main :: IO ()
 main = mainWith (d # frame 0.2) >> putStrLn "Done!"
 
+d1 = e # snugR <> ( e # translateY (sqrt 2 / 2)) # snugL
 
-d = w1 <> w2
+d = d1 # snugB <> d1 # snugT
+
+e = w1 <> w2
   where
     w1 = p1 (bn31) (reverse bn31) # centerXY # reflectX
     w2 = p1 jamaicanDutchBeach (reverse jamaicanDutchBeach) # reflectY # rotateBy (1/4) # centerXY 
@@ -22,12 +25,12 @@ d = w1 <> w2
 p1 :: Colours -> Colours -> Diagram B
 p1 c1 c2 = s1 ||| s2
   where
-    n = 6
+    n = 3
     s1 = (hcat (side n c1)) # rotateBy (1/8) # reflectY # centerXY
     s2 = (hcat (side n c2)) # rotateBy (1/8) # reflectX # centerXY
 
 
 side n colours
  -- = zipWith (\r c -> rect 1 (r**(1/2)) # alignB # lw none # fc c) [1..n] (cycle colours)
- = zipWith (\r c -> rect 1 (r+r) # alignB # lw none # fc c) [1..n] (cycle colours)
+ = zipWith (\r c -> rect 1 (r) # alignB # lw none # fc c) [1..n] (cycle colours)
 
