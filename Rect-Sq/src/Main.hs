2b01d1e5c385fe99e8e54f5828114551e9c8e309
silky <noonsilk+-noonsilk@gmail.com>
2020-01-02 10:03:06 +1100

    mm

--- Rect-Sq/src/Main.hs
+++ Rect-Sq/src/Main.hs
@@ -14,7 +14,7 @@ main = mainWith (d # frame 0.2) >> putStrLn "Done!"
 
 d1 = t1 <> t2
   where
-    t1 = e bn31 jamaicanDutchBeach # snugR 
+    t1 = e bn31 pisos # snugR 
     t2 = e summertime barbapapa # translateY (sqrt 2 / 2) # snugL
 
 d = d1 # snugB <> d1 # reflectX # reflectY # snugT 
@@ -28,12 +28,12 @@ e c1 c2 = w1 <> w2
 p1 :: Colours -> Colours -> Diagram B
 p1 c1 c2 = s1 ||| s2
   where
-    n = 3
+    n = 2
     s1 = (hcat (side n c1)) # rotateBy (1/8) # reflectY # centerXY
     s2 = (hcat (side n c2)) # rotateBy (1/8) # reflectX # centerXY
 
 
 side n colours
  -- = zipWith (\r c -> rect 1 (r**(1/2)) # alignB # lw none # fc c) [1..n] (cycle colours)
- = zipWith (\r c -> rect 1 (r) # alignB # lw none # fc c) [1..n] (cycle colours)
+ = zipWith (\r c -> rect 1 r # alignB # lw none # fc c) [1..n] (cycle colours)
 
